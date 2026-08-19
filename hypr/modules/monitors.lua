e38da6fdffcb2c721f8d05968a7a2d60e6ce13f1
HarmlessValve <Altagemilang@protonmail.com>
2026-08-19 21:18:34 +0700

    feat: add monitor duplicate config

--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -1,4 +1,13 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "1",
+    mirror   = "eDP-1",
+})
+
 hl.monitor({
     output   = "",
     mode     = "preferred",
